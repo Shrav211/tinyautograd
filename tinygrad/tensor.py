8543cb17db324b5f472501b1931fd28c0a1c4b09
Shrav211 <shravanshan211@gmail.com>
2025-12-25 21:30:10 -0600

    added neg, sub, mul and pow

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -42,10 +42,87 @@ class Tensor:
                 self.grad += out.grad
 
             if other.requires_grad:
-                if self.grad is None: other.grad = 0
+                if other.grad is None: other.grad = 0
                 other.grad += out.grad
 
         out._backward = _backward
         return out
+    
+    def __mul__(self, other):
+        
+        out = Tensor(self.data * other.data, requires_grad=self.requires_grad or other.requires_grad)
+        out._prev = {self, other}
 
+        def _backward():
+            if not out.requires_grad:
+                return 
+            
+            if self.requires_grad:
+                if self.grad is None: self.grad = 0
+                self.grad += out.grad * other.data
+
+            if other.requires_grad:
+                if other.grad is None: other.grad = 0
+                other.grad += out.grad * self.data
+
+        out._backward = _backward
+        return out
+
+    def __neg__(self):
+
+        out = Tensor((-1) * self.data, requires_grad=self.requires_grad)
+        out._prev = {self}
+
+        def _backward():
+            if not out.requires_grad:
+                return
+            
+            if self.requires_grad:
+                if self.grad is None: self.grad = 0
+                self.grad += (-1) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __sub__(self, other):
+
+        out = Tensor(self.data - other.data, requires_grad=self.requires_grad or other.requires_grad)
+        out._prev = {self, other}
+
+        def _backward():
+            if not out.requires_grad:
+                return
+            
+            if self.requires_grad:
+                if self.grad is None: self.grad = 0
+                self.grad += out.grad
+
+            if other.requires_grad:
+                if other.grad is None: other.grad = 0
+                other.grad += (-1) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __pow__(self, p):
+
+        out = Tensor(self.data ** p, requires_grad=self.requires_grad)
+        out._prev = {self.other}
+
+        def _backward():
+            if not out.requires_grad:
+                return
+            
+            if self.requires_grad:
+                if self.grad is None: self.grad = 0
+                self.grad += out.grad * p * (self.data ** (p-1))
+
+        out._backward = _backward
+        return out
+
+x = Tensor(2, True)
+y = Tensor(3, True)
+z = x - y
+z.backward()
+print(x.grad, y.grad)
 
