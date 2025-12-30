db293272ebf2644bff61d4a2c5db02fd07d690b9
Shrav211 <shravanshan211@gmail.com>
2025-12-30 15:15:29 -0600

    fixed bug in relu

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -180,7 +180,7 @@ class Tensor:
                 self.__init_grad()
                 mask = (self.data > 0).astype(self.data.dtype)
                 grad_self = out.grad * mask
-                self.data += _unbroadcast(grad_self, self.data.shape)
+                self.grad += _unbroadcast(grad_self, self.data.shape)
 
         out._backward = _backward
         return out
